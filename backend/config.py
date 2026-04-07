176bf0a89ce7acabee62314b654947addd1334b5
Petro Kushchak <v-Petro.Kushchak@lionbridge.com>
2026-04-07 15:44:51 +0300

    default port

--- backend/config.py
+++ backend/config.py
@@ -7,7 +7,7 @@ class Config:
     def __init__(self):
         self.pools_config = os.getenv("POOLS_CONFIG", "/data/pools.json")
         self.scheduler_interval = int(os.getenv("SCHEDULER_INTERVAL", "60"))
-        self.api_port = int(os.getenv("API_PORT", "8000"))
+        self.api_port = int(os.getenv("API_PORT", "8083"))
         self.build_version = os.getenv("BUILD_VERSION", "unknown")
         self.version_file = os.getenv("VERSION_FILE", "/app/VERSION")
 
